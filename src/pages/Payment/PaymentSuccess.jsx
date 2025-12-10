17e5ce72077ab911b463fd3953915b34964bcec3
roufrubelbd <roufrubelbd@gmail.com>
2025-12-10 20:22:30 +0000

    auth updateddg

--- src/pages/Payment/PaymentSuccess.jsx
+++ src/pages/Payment/PaymentSuccess.jsx
@@ -1,20 +1,9 @@
-import { useEffect } from "react";
-// import useAuth from "../../hooks/useAuth";
-import axios from "axios";
-import { Link, useSearchParams } from "react-router";
+
+import { Link } from "react-router";
+
 
 const PaymentSuccess = () => {
-  // const { user } = useAuth();
 
-  const [searchParams] = useSearchParams()
-  const sessionId = searchParams.get('session_id')
-  useEffect(() => {
-    if (sessionId) {
-      axios.post(`${import.meta.env.VITE_API_URL}/payment-success`, {
-        sessionId,
-      })
-    }
-  }, [sessionId])
 
   return (
     <div className="text-center mt-10 min-h-screen flex flex-col items-center">
